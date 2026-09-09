d46ae759f81a2ac98a0edb635416f3726002512c
sashaoxel <sashaoxel@sashaoxels-MacBook-Air.local>
2026-09-10 00:30:39 +0800

    Реализация первой лабораторной работы по дисциплине ООП

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,30 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+//Пользователь вводит предложение, отсортировать слова по длине слов и в случае совпадения длины отсортировать по алфавиту.
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String[] args) {
+        System.out.print("Введите ваше предложение: ");
+        Scanner scanner = new Scanner(System.in);
+        var userInput = scanner.nextLine().split(" ");
+        String temp;
+
+        for (int i = 0; i < userInput.length - 1; i++) {
+                if (userInput[i].length() > userInput[i+1].length()) {
+                    temp = userInput[i+1];
+                    userInput[i+1] = userInput[i];
+                    userInput[i] = temp;
+                } else if (userInput[i].length()==userInput[i+1].length()) {
+                    if (userInput[i].compareTo(userInput[i+1]) > 0) {
+                        temp = userInput[i+1];
+                        userInput[i+1] = userInput[i];
+                        userInput[i] = temp;
+                    }
+                }
+        }
+
+        IO.println(Arrays.toString(userInput));
+
     }
 }
